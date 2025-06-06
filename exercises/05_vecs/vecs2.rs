80e554ac46ae1456c836c561e67b87136f6fdb26
DevShiba <migerushiba@gmail.com>
2025-06-05 23:29:46 -0300

    feat(exercises): solving semantics exercises

--- exercises/05_vecs/vecs2.rs
+++ exercises/05_vecs/vecs2.rs
@@ -4,6 +4,8 @@ fn vec_loop(input: &[i32]) -> Vec<i32> {
     for element in input {
         // TODO: Multiply each element in the `input` slice by 2 and push it to
         // the `output` vector.
+        let new_value = element * 2;  
+        output.push(new_value); // This is the solution to the exercise.
     }
 
     output
@@ -24,7 +26,7 @@ fn vec_map(input: &[i32]) -> Vec<i32> {
     input
         .iter()
         .map(|element| {
-            // ???
+           element * 2 // Multiply each element by 2. This is the solution to the exercise.
         })
         .collect()
 }
--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -20,7 +20,7 @@ mod tests {
     fn move_semantics2() {
         let vec0 = vec![22, 44, 66];
 
-        let vec1 = fill_vec(vec0);
+        let vec1 = fill_vec(vec0.clone()); // Clone `vec0` to make it accessible after the function call. This solves the exercise.
 
         assert_eq!(vec0, [22, 44, 66]);
         assert_eq!(vec1, [22, 44, 66, 88]);
